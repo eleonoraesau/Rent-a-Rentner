d5997844cc3b1fd0d43248e92b78d5eb2ce29a4c
Jan Scheller <scheller.jan1992@gmail.com>
2022-08-23 12:45:59 +0200

    build new and create

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,6 @@
 class OffersController < ApplicationController
   before_action :set_offer, only: :show
+  # before_action :set_user, only: [:new, :create]
 
   def index
     @offers = Offer.all
@@ -14,9 +15,11 @@ class OffersController < ApplicationController
 
   def create
     @offer = Offer.new(offer_params)
-    if @offer.save
+    @offer.user = current_user
+    if @offer.save!
       redirect_to @offer, notice: "Your offer was successfull created"
     else
+      # raise
       render :new, status: :unprocessable_entity
     end
   end
@@ -26,6 +29,10 @@ class OffersController < ApplicationController
   def set_offer
     @offer = Offer.find(params[:id])
   end
+
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
   def offer_params
     params.require(:offer).permit(:name, :location, :description, :category, :availability)
