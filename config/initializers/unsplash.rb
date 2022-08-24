58665141aa2be86c73f912792747dc47d9aee1bf
Fabian Schmidt <fabian@my-schmidt.de>
2022-08-24 12:39:59 +0200

    add unsplash gem

---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,6 @@
+Unsplash.configure do |config|
+  config.application_access_key = "dLoxjdZR5uDYZ8PGbvtJcqDMU3Y4Kqgkz5jPiiWPGXI"
+  config.application_secret = "eKzX6AE2DWIuPT67X2sVaFuiF10us8Qg71i6xu5R3wA"
+  config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
+  config.utm_source = "rent-a-rentner"
+end
