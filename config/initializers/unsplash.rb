a8ea18e514364f788f5a959693b0826ed64d87fd
Fabian Schmidt <fabian@my-schmidt.de>
2022-08-24 15:01:58 +0200

    hid keys

--- config/initializers/unsplash.rb
+++ config/initializers/unsplash.rb
@@ -1,6 +1,6 @@
 Unsplash.configure do |config|
-  config.application_access_key = "dLoxjdZR5uDYZ8PGbvtJcqDMU3Y4Kqgkz5jPiiWPGXI"
-  config.application_secret = "eKzX6AE2DWIuPT67X2sVaFuiF10us8Qg71i6xu5R3wA"
+  config.application_access_key = ENV.fetch["UNSPLASH_ACCES_KEY"]
+  config.application_secret = ENV.fetch["UNSPLASH_SECRET_KEY"]
   config.application_redirect_uri = "urn:ietf:wg:oauth:2.0:oob"
   config.utm_source = "rent-a-rentner"
 end
