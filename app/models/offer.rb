b01adc820efc184111a9f381009a52f13a1a5404
Jan Scheller <scheller.jan1992@gmail.com>
2022-08-25 13:05:50 +0200

    search engine

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,4 +5,11 @@ class Offer < ApplicationRecord
   validates :name, :location, :description, :category, :availability, presence: true
   validates :category, inclusion: { in: CATEGORIES }
   validates :availability, inclusion: { in: WEEKDAYS}
+
+  include PgSearch::Model
+  pg_search_scope :search_by_category_and_availability_and_location,
+    against: [ :category, :availability, :location ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
